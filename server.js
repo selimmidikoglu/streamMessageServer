9b6c8dbcaf28411b0ee1493b322ffb4d96027a73
selimmidikoglu <selim.midikoglu@ug.bilkent.edu.tr>
2019-05-10 01:38:31 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+const port = 3000;
+
+io.on("connection", socket => {
+    console.log('a user connected :D',socket.id);
+    socket.on("chat message", msg => {
+        console.log(msg);
+        io.emit("chat message",msg);
+    });
+});
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
